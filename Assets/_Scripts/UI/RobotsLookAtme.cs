53ddea787cb10adcd114099acf9260ff4d742903
yeonyeong0120 <yeonyeong0120@gmail.com>
2025-12-10 14:13:30 +0900

    Add : 안내 로봇 기반 구현

---
+++ Assets/_Scripts/UI/RobotsLookAtme.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SimpleLookAt : MonoBehaviour
+{
+    private Transform target;
+
+    void Update()
+    {
+        // VR 카메라(내 눈)를 찾음
+        if (target == null)
+        {
+            var cam = Camera.main; // 혹은 FindObjectOfType<Camera>();
+            if (cam != null) target = cam.transform;
+        }
+
+        if (target != null)
+        {
+            // 타겟을 바라보되, Y축(위아래) 회전은 억제하고 싶다면 아래 주석 해제
+            // Vector3 targetPos = new Vector3(target.position.x, transform.position.y, target.position.z);
+            // transform.LookAt(targetPos);
+
+            transform.LookAt(target); // 그냥 쳐다보기
+            transform.Rotate(0, 180, 0);
+        }
+    }
+}

9efe3058f3512f6750cc14008103c793c7ffabea
yeonyeong0120 <yeonyeong0120@gmail.com>
2025-12-11 14:52:40 +0900

    Fix : 로봇 본체 오류까진 고침

--- Assets/_Scripts/UI/RobotsLookAtme.cs
+++ Assets/_Scripts/UI/RobotsLookAtme.cs
@@ -18,8 +18,11 @@ public class SimpleLookAt : MonoBehaviour
             // 타겟을 바라보되, Y축(위아래) 회전은 억제하고 싶다면 아래 주석 해제
             // Vector3 targetPos = new Vector3(target.position.x, transform.position.y, target.position.z);
             // transform.LookAt(targetPos);
+            // 사용자 눈높이랑 맞춤,,
+            Vector3 targetPos = new Vector3(target.position.x, transform.position.y, target.position.z);
+            transform.LookAt(targetPos);
 
-            transform.LookAt(target); // 그냥 쳐다보기
+            //transform.LookAt(target); // 그냥 쳐다보기
             transform.Rotate(0, 180, 0);
         }
     }
